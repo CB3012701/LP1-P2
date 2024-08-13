cce68264cec901aa6288a3d502d8fe2a70ec4fb1
Érica Barbosa <82848078+CB3012701@users.noreply.github.com>
2024-08-13 18:08:24 -0300

    Update TestarConsultaAgendada.java

--- P2/TestarConsultaAgendada.java
+++ P2/TestarConsultaAgendada.java
@@ -9,7 +9,7 @@ public class TestarConsultaAgendada {
 
         List<String> conteudoArquivo = new ArrayList();
 
-        ConsultaAgendada p1 = new ConsultaAgendada(10, 15, 30, 14, 10, 2023, "Erica", "Marcos");
+        ConsultaAgendada p1 = new ConsultaAgendada(10, 15, 30, 14, 10, 2023, "Erica", "Maria");
         System.out.println("---------- AGENDAMENTO DE CONSULTA ----------");
         conteudoArquivo.add("---------- AGENDAMENTO DE CONSULTA ----------");
         System.out.println("Nome do paciente: " + p1.getNomePaciente());
